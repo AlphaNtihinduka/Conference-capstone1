3552d50aa771bf99f8206122b70ad0ae4e741207
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-04-13 18:17:26 +0300

    Fix linter errors

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-const humberger = document.querySelector(".fa-bars");
-const times_icon = document.querySelector(".fa-times");
-const slideBar = document.querySelector(".slide-bar_mobile");
-const timesIcon = document.querySelector(".fa-times");
-const slideLi = document.querySelectorAll(".header_items_mobile li")
+const humberger = document.querySelector('.fa-bars');
+const timesIcon = document.querySelector('.fa-times');
+const slideBar = document.querySelector('.slide-bar_mobile');
+const slideLi = document.querySelectorAll('.header_items_mobile li');
 
-humberger.addEventListener("click", function(){
-    slideBar.style.left="0";
-    console.log("hambuger")
-})
-times_icon.addEventListener("click", function(){
-    slideBar.style.left="-100%";
-})
+humberger.addEventListener('click', () => {
+  slideBar.style.left = '0';
+});
+timesIcon.addEventListener('click', () => {
+  slideBar.style.left = '-100%';
+});
 
 slideLi.forEach((n) => n.addEventListener('click', () => {
-    slideBar.style.left="-100%";
-  }));
+  slideBar.style.left = '-100%';
+}));
